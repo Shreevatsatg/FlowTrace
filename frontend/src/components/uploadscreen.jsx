83eb74c5f5e410dc8ca247a7e69248a1e0b35bad
Shreevatsatg <tgshreevatsa@gmail.com>
2026-02-19 20:30:54 +0530

    bug fix and added .env

--- frontend/src/components/uploadscreen.jsx
+++ frontend/src/components/uploadscreen.jsx
@@ -1,6 +1,7 @@
 
 import { useState,useRef } from "react";
-const API_BASE = "http://localhost:3000";
+
+const API_BASE = import.meta.env.VITE_API_BASE
 
 export default function UploadScreen({ onAnalyze }) {
   const [file, setFile]       = useState(null);
